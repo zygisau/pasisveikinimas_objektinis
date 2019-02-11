aea712f795b091a3a970bf6941d0b656d156981e
zygisau <zygimantasaugunas@gmail.com>
2019-02-11 12:41:12 +0200

    Pirmine uzduotis

--- objektinis.cpp
+++ objektinis.cpp
@@ -4,40 +4,59 @@
 #include <stdio.h>
 using namespace std;
 //
-void spausdinti(int dydis, string vardas);
+void spausdinti(string vardas);
 //
 int main()
 {
     string vardas;
-    int dydis;
     //
     printf("Koks Jusu vardas: ");
     cin >> vardas;
-    printf("Iveskite remelio dydi (nuo 1 iki 10): ");
-    cin >> dydis;
-    spausdinti(dydis, vardas);
+    spausdinti(vardas);
     system("PAUSE");
     return 0;
 }
 //
-void spausdinti(int dydis, string vardas) {
-    int plotis = dydis*2 + 10 + vardas.length();
-    
-    int ilgis = dydis*2 + 1;
+void spausdinti(string vardas) {
+    string eilute1, eilute2, eilute3, eilute4, eilute5;
+    int plotis =  12 + vardas.length();
+    int ilgis = 3;
     for (int i=1; i<=plotis+2; i++) { // pirma *** eilute
-        printf("*");
+        eilute1 = eilute1 + '*';
     }
-    printf("\n");
-    for (int i=1; i<=ilgis; i++) {
-
-        if(i==dydis+1) {
-            printf("%*cSveikas, %s!%*c\n", -(dydis+1), '*', vardas.c_str(), dydis+1, '*');
-        } else {
-            printf("%c%*c\n", '*', plotis+1, '*');
-        }
+    cout  << eilute1 << endl;
+    //
+    eilute2 = eilute2 + '*'; // tuscia *** eilute
+    for (int i=1; i<=plotis; i++) { 
+        eilute2 = eilute2 + ' ';
     }
+    eilute2 = eilute2 + '*';
+    cout  << eilute2 << endl;
+    //
+    eilute3 = eilute3 + "* Sveikas, " + vardas + "! *";
+    cout  << eilute3 << endl;
+    //
+    eilute4 = eilute4 + '*'; // tuscia *** eilute
+    for (int i=1; i<=plotis; i++) { 
+        eilute4 = eilute4 + ' ';
+    }
+    eilute4 = eilute4 + '*';
+    cout  << eilute4 << endl;
+    //
     for (int i=1; i<=plotis+2; i++) { // paskutine *** eilute
-        printf("*");
+        eilute5 = eilute5 + '*';
     }
+    cout  << eilute5 << endl;
+    // printf("\n");
+    // for (int i=1; i<=ilgis; i++) {
+    //     if(i==dydis+1) {
+    //         printf("%*cSveikas, %s!%*c\n", -(dydis+1), '*', vardas.c_str(), dydis+1, '*');
+    //     } else {
+    //         printf("%c%*c\n", '*', plotis+1, '*');
+    //     }
+    // }
+    // for (int i=1; i<=plotis+2; i++) { // paskutine *** eilute
+    //     printf("*");
+    // }
     printf("\n");
 }
